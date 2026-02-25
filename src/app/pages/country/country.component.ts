e5e4ff37f98a45996b7cfebcded24e73abf8500b
Anthony Gorski <anthony.developpeurweb@gmail.com>
2026-02-25 09:58:45 +0100

    feat(country.component): prevent memory leaks with takeUntilDestroyed

--- src/app/pages/country/country.component.ts
+++ src/app/pages/country/country.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject, DestroyRef } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router } from '@angular/router';
 import Chart from 'chart.js/auto';
 import { OlympicService } from 'src/app/services/olympic.service';
@@ -12,6 +13,7 @@ import { DataService } from 'src/app/services/data.service';
 export class CountryComponent implements OnInit {
   public lineChart!: Chart;
   public error!: string;
+  private destroyRef = inject(DestroyRef);
 
   constructor(
     private route: ActivatedRoute,
@@ -23,35 +25,38 @@ export class CountryComponent implements OnInit {
   ngOnInit() {
     const countryId = this.route.snapshot.paramMap.get('id');
     if (countryId) {
-      this.dataService.getOlympicsByCountry(countryId).subscribe({
-        next: (country: any) => {
-          if (country) {
-            this.olympicService.updateHeaderData(country.country, [
-              {
-                label: 'Number of entries',
-                value: country.participations.length,
-              },
-              { label: 'Total Number of medals', value: country.totalMedals },
-              {
-                label: 'Total Number of athletes',
-                value: country.totalAthletes,
-              },
-            ]);
+      this.dataService
+        .getOlympicsByCountry(countryId)
+        .pipe(takeUntilDestroyed(this.destroyRef))
+        .subscribe({
+          next: (country: any) => {
+            if (country) {
+              this.olympicService.updateHeaderData(country.country, [
+                {
+                  label: 'Number of entries',
+                  value: country.participations.length,
+                },
+                { label: 'Total Number of medals', value: country.totalMedals },
+                {
+                  label: 'Total Number of athletes',
+                  value: country.totalAthletes,
+                },
+              ]);
 
-            const years = country.participations.map((p: any) => p.year);
-            const medals = country.participations.map((p: any) =>
-              p.medalsCount.toString(),
-            );
+              const years = country.participations.map((p: any) => p.year);
+              const medals = country.participations.map((p: any) =>
+                p.medalsCount.toString(),
+              );
 
-            this.buildChart(years, medals);
-          } else {
-            this.router.navigateByUrl('not-found');
-          }
-        },
-        error: (error) => {
-          this.error = error.message;
-        },
-      });
+              this.buildChart(years, medals);
+            } else {
+              this.router.navigateByUrl('not-found');
+            }
+          },
+          error: (error) => {
+            this.error = error.message;
+          },
+        });
     }
   }
 
