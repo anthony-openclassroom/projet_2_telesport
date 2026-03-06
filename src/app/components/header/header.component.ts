7c6ca81f3eefd1de1df70b3be62b56a58a98bc31
Anthony Gorski <anthony.developpeurweb@gmail.com>
2026-03-06 17:47:26 +0100

    feat(header.component.ts): implement automatic unsubscription using takeUntilDestroyed to prevent memory leaks in observables

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { OlympicService } from '../../services/olympic.service';
 import { KPI } from '../../models/kpi';
 
@@ -10,19 +11,24 @@ import { KPI } from '../../models/kpi';
 export class HeaderComponent implements OnInit {
   public titlePage: string = '';
   public kpis: KPI[] = [];
+  private destroyRef = inject(DestroyRef);
 
   // Injection du service OlympicService pour accéder aux données du header
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     // Souscription aux observables du service pour mettre à jour le titre
-    this.olympicService.title$.subscribe((title) => {
-      // ⬆️ Le suffixe $ (Finnish notation) indique que c'est un Observable, alors que 'title' est la valeur extraite de ce flux.
-      this.titlePage = title;
-    });
+    this.olympicService.title$
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((title) => {
+        // ⬆️ Le suffixe $ (Finnish notation) indique que c'est un Observable, alors que 'title' est la valeur extraite de ce flux.
+        this.titlePage = title;
+      });
     // Et les KPIs du header
-    this.olympicService.kpis$.subscribe((kpis) => {
-      this.kpis = kpis;
-    });
+    this.olympicService.kpis$
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((kpis) => {
+        this.kpis = kpis;
+      });
   }
 }
